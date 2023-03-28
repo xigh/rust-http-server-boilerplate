89f0dcdb29efdf4bd918fe0448e345201838ebaf
Philippe Anel <zexigh@gmail.com>
2023-03-28 23:01:57 +0200

    add getopt

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,99 @@
-use hyper::{Body, Method, Request, Response, StatusCode, server::Server};
-use hyper::service::{make_service_fn, service_fn};
-use std::path::Path;
-use tokio::fs;
-use log::{info, warn, debug, error, LevelFilter};
 use env_logger::{Builder, Target};
-use std::fs::{OpenOptions};
+use getopts::Options;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{server::Server, Body, Method, Request, Response, StatusCode};
+use log::{debug, error, info, warn};
+use std::env;
+use std::fs::OpenOptions;
 use std::io::Write;
-
+use std::net::SocketAddr;
+use std::path::Path;
+use std::str::FromStr;
+use tokio::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optflag("?", "help", "Show this help")
+        .optflag("h", "help", "Show this help")
+        .optopt("v", "verbosity", "Set the log level", "LEVEL")
+        .optopt("a", "addr", "Set the address", "ADDR")
+        .optopt("l", "logfile", "Set the logfile", "LOGFILE");
+
+    let show_usage = || {
+        let prog_name = args.first().unwrap();
+        let prog_name = Path::new(prog_name);
+        let prog_name = prog_name.file_name().unwrap();
+        let usage = opts.short_usage(prog_name.to_str().unwrap());
+        eprintln!("{}", usage);
+    };
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("{}", e);
+            show_usage();
+            return Ok(());
+        }
+    };
+
+    let log_level = matches
+        .opt_str("v")
+        .unwrap_or_else(|| String::from("info"))
+        .parse::<log::LevelFilter>()
+        .unwrap();
+
+    let addr = matches
+        .opt_str("a")
+        .unwrap_or_else(|| String::from("127.0.0.1:8080"));
+
+    let log_file = matches
+        .opt_str("l")
+        .unwrap_or_else(|| String::from("debug.log"))
+        .parse::<String>()
+        .unwrap();
+
+    if matches.opt_present("?") || matches.opt_present("h") {
+        show_usage();
+        return Ok(());
+    }
 
     let log_file = OpenOptions::new()
         .write(true)
         .append(true)
         .create(true)
-        .open("debug.log")
+        .open(log_file.as_str())
         .unwrap();
 
     let mut builder = Builder::new();
-        builder
-            .format(|buf, record| {
-                writeln!(
-                    buf,
-                    "[{}] [{}] {}",
-                    record.level(),
-                    record.target(),
-                    record.args()
-                )
-            })
-            .filter(None, LevelFilter::Info)
-            .target(Target::Pipe(Box::new(log_file)))
-            .init();
+    builder
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[{}] [{}] {}",
+                record.level(),
+                record.target(),
+                record.args()
+            )
+        })
+        .filter(None, log_level)
+        .target(Target::Pipe(Box::new(log_file)))
+        .init();
 
     // Create a new hyper server that listens on port 8000
-    let addr = ([127, 0, 0, 1], 8000).into();
-    let make_service = make_service_fn(|_conn| async {
-        Ok::<_, hyper::Error>(service_fn(handle_request))
-    });
-    let server = Server::bind(&addr).serve(make_service);
+    let socket_addr = match SocketAddr::from_str(addr.as_str()) {
+        Ok(addr) => addr,
+        Err(e) => panic!("Error parsing address: {}", e),
+    };
+
+    let make_service =
+        make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(handle_request)) });
+    let server = Server::bind(&socket_addr).serve(make_service);
 
     // Run the server
-    info!("Listening on http://{}", addr);
+    info!("Listening on http://{}", socket_addr);
     if let Err(e) = server.await {
         error!("server error: {}", e);
     }
@@ -56,7 +108,9 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
     debug!("{method} {uri_path}");
     match (method, uri_path) {
         (&Method::GET, "/hello" | "/hello/") => handle_hello(),
-        (&Method::GET, _) if uri_path.starts_with("/api/v1/") => handle_api_v1(&uri_path[8..].trim_end_matches('/')),
+        (&Method::GET, _) if uri_path.starts_with("/api/v1/") => {
+            handle_api_v1(&uri_path[8..].trim_end_matches('/'))
+        }
         _ => serve_file(uri_path).await,
     }
 }
@@ -67,7 +121,10 @@ fn handle_hello() -> Result<Response<Body>, hyper::Error> {
 
 fn handle_api_v1(sub_path: &str) -> Result<Response<Body>, hyper::Error> {
     warn!("api-v1");
-    Ok(Response::new(Body::from(format!("API v1 resource: {}", sub_path))))
+    Ok(Response::new(Body::from(format!(
+        "API v1 resource: {}",
+        sub_path
+    ))))
 }
 
 async fn serve_file(path: &str) -> Result<Response<Body>, hyper::Error> {
